92fdac15de8230016e743cdd12f2e2574e5a1d63
artem_ <artem_5511@mail.ru>
2023-04-12 23:00:42 +0300

    02 test

---
+++ src/02/02.ts
@@ -0,0 +1,16 @@
+const student = {
+    "name": "Dimych",
+    age: 32,
+    isActive: false,
+    address: {
+        streetTitle: "Surganova 2",
+        city: {
+            title: "Minsk",
+            contryTitle: "Belarus"
+        }
+    }
+}
+
+console.log(student.age)
+console.log(student.name)
+console.log(student.address.city.title)
